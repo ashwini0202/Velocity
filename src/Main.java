5c303df6d5fc0cfdf121a81b5a6232e278634438
Ashwini Biradar <ashwinichambole@gmail.com>
2024-09-23 20:21:59 +0200

    First mesage

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList <String> array= new ArrayList<>();
+        array.add("Ashwini");
+        array.add("praveen");
+        array.add("Anajli");
+
+        System.out.println(array);
+    }
+}
